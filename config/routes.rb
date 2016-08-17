2fbd41cc831de9d11876b6ea31f3e7a016fab4a7
Justin Roberts <justin.roberts@salesforce.com>
2016-08-17 08:41:23 -0400

    adding some table classes, root is now current beans

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   resources :origins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "beans#index"
+  root to: "beans#current"
 
 end
